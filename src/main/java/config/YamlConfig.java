3f21413d00e05b7e5b060f7e5a844d4499606935
shaofan <yanggang@diligrp.com>
2020-03-31 20:47:04 +0800

    * 转换maven

--- src/main/java/config/YamlConfig.java
+++ src/main/java/config/YamlConfig.java
@@ -1,23 +1,26 @@
 package config;
 
+import cn.hutool.core.io.resource.ClassPathResource;
 import com.esotericsoftware.yamlbeans.YamlReader;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
 
 
 public class YamlConfig {
 
     public static final YamlConfig config = fromFile("config.yaml");
-    
+
     public List<WorldConfig> worlds;
     public ServerConfig server;
 
     public static YamlConfig fromFile(String filename) {
         try {
-            YamlReader reader = new YamlReader(new FileReader(filename));
+            ClassPathResource classPathResource = new ClassPathResource(filename);
+            YamlReader reader = new YamlReader(classPathResource.getReader(Charset.defaultCharset()));
             YamlConfig config = reader.read(YamlConfig.class);
             reader.close();
             return config;
